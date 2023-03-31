d0346e8a5dc7365040530d905ff65c1a05aedfbc
Panha <turtoisecool@gmail.com>
2023-03-31 19:44:02 +0700

    Nice

--- C_Programming/main.c
+++ C_Programming/main.c
@@ -2,9 +2,9 @@
 #include <conio.h>
 
 int main(){
-    printf("I like this git hub thingy.")
-
-    printf("Git hub is cool but it's killing me rn.")
+    printf("I like this git hub thingy.");
+    
+    printf("Git hub is cool but it's killing me rn.");
     
     // getch();
     return 0;
