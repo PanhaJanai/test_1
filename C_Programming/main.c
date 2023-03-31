f7b43c50dfca5e88ece9d4526c51809f584c464c
Panha <turtoisecool@gmail.com>
2023-03-31 19:26:37 +0700

    Please spare me.

---
+++ C_Programming/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main(){
+    printf("I like this git hub thingy.")
+
+    printf("Git hub is cool but it's killing me rn.")
+    
+    // getch();
+    return 0;
+}
