6b8ea77e7626edbd07528aa2dfd07fada9d62d46
Panha <turtoisecool@gmail.com>
2023-05-30 08:11:37 +0700

    remove some text

--- C_Programming/calculator.c
+++ C_Programming/calculator.c
@@ -10,7 +10,7 @@ int main(){
     int num1, num2;
     char op;
 
-    printf("Welcome to calculator. What would you like to do?\n");
+    printf("Welcome to calculator.\n");
 
     printf("Enter + for addition\nEnter - for subtraction\nEnter * for multiplication\nEnter / for division\nPlease enter your choice: ");
 
