6923f5ad38809d16eeca99ae36e9ace430b3bb53
Panha <turtoisecool@gmail.com>
2023-05-30 07:52:57 +0700

    Made user prompt for operator

---
+++ C_Programming/calculator.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main(){
+    int num1, num2;
+    char op;
+
+    printf("Welcome to calculator! What would you like to do?\n");
+
+    printf("Enter + for addition\nEnter - for subtraction\nEnter * for multiplication\nEnter / for division\nPlease enter your choice: ");
+
+    scanf("%c", &op);
+
+    
+    // getch();
+    return 0;
+}
